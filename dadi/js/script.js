1017fa59ceda77f9cd452ac0ba4752862a76132d
serena-peschiaroli <serena.peschiaroli@gmail.com>
2023-10-23 16:36:07 +0200

    Adding: p html

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -12,9 +12,13 @@ function GetBetNumber (min, max) {
 
 const CpuBet = GetBetNumber (1, 6);
 console.log (" Cpu Bet" , CpuBet);
+document.getElementById("CpuBet").innerHTML = `il computer estrae ${CpuBet}`;
+
+
 
 const UserBet= GetBetNumber ( 1, 6);
 console.log( " User Bet " , UserBet);
+document.getElementById("UserBet").innerHTML = `Tu hai estratto ${UserBet}`
 
 let UserWin = false;
 let CpuWin = false;
@@ -41,8 +45,58 @@ for (let i = 0; i < betNumber.length; i++) {
 
 if (UserWin === true && CpuWin === false) {
     console.log("Hai vinto");
-}else if (UserWin === true && CpuWin === true || UserWin === false && CpuWin === false) {
+    document.getElementById("Whowin").innerHTML = (`hai vinto!`)
+}else if (UserWin === true && CpuWin === true ) {
     console.log("Pareggio");
+    document.getElementById("Whowin").innerHTML = (`Pareggio!`)
 }else if (UserWin === false && CpuWin === true){
     console.log("Ha vinto il computer");
+    document.getElementById("Whowin").innerHTML = (`hai perso!`)
+}else {
+    console.log ("Avete perso")
+    document.getElementById("Whowin").innerHTML = (`Non ha vinto nessuno, ritenta!`)
 }
+
+
+
+// const betNumber = [1, 2, 3, 4, 5, 6];
+
+// function GetBetNumber(min, max) {
+//     min = Math.ceil(min);
+//     max = Math.floor(max);
+//     return Math.floor(Math.random() * (max - min + 1) + min);
+// }
+
+// const CpuBet = GetBetNumber(1, 6);
+// console.log("Cpu Bet:", CpuBet);
+
+// const UserBet = GetBetNumber(1, 6);
+// console.log("User Bet:", UserBet);
+
+// let UserWin = false;
+// let CpuWin = false;
+
+// for (let i = 0; i < betNumber.length; i++) {
+//     const WinBet = betNumber[i];
+
+//     if (UserBet > CpuBet) {
+//         UserWin = true;
+//         CpuWin = false;
+//     } else if (UserBet < CpuBet) {
+//         CpuWin = true;
+//         UserWin = false;
+//     } else {
+       
+//     }
+
+//     console.log("User Bet:", UserBet);
+//     console.log("Cpu Bet:", CpuBet);
+
+//     if (UserWin) {
+//         console.log("Hai vinto!");
+//     } else if (UserWin === CpuWin) {
+//         console.log("Pareggio");
+//     } else {
+//         console.log("Ha vinto il computer");
+//     }
+// }
