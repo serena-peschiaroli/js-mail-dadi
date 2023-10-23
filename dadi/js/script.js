a6cc9471a66baeb2659faa52864b0e2b84d72793
serena-peschiaroli <serena.peschiaroli@gmail.com>
2023-10-23 14:37:30 +0200

    Add: scaffholding for dadi js

---
+++ dadi/js/script.js
@@ -0,0 +1,5 @@
+/* dati: numero array da 1-6; numero random generato per cpu; numero random generato per giocatore; 
+vince chi fa il numero più alto*/
+
+const betNumber= [1 , 2 , 3 , 4 , 5 , 6];
+
